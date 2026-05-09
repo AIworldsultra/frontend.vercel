ecdc10e944a821b1c928211f1ce350568b6bbff0
raahialt1-png <raahialt1@gmail.com>
2026-05-08 19:08:30 -0700

    Initialize React app with routing setup

---
+++ app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Callback from './Callback';
+import Dashboard from './Dashboard';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
